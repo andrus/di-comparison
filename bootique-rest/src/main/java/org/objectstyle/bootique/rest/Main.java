65528df4a395a0cd74d8b6fb876f826172330665
Andrus Adamchik <andrus@objectstyle.com>
2022-02-03 14:12:29 +0200

    Adding REST modules

--- bootique-rest/src/main/java/org/objectstyle/bootique/rest/Main.java
+++ bootique-rest/src/main/java/org/objectstyle/bootique/rest/Main.java
@@ -2,9 +2,18 @@ package org.objectstyle.bootique.rest;
 
 import io.bootique.Bootique;
 
+import java.lang.management.ManagementFactory;
+import java.time.LocalDateTime;
+
 public class Main {
 
     public static void main(String[] args) {
+        // measuring JVM start time
+        System.out.println(ManagementFactory.getRuntimeMXBean().getUptime());
+
+        // timestamping the app start point
+        System.out.println(LocalDateTime.now());
+
         Bootique.app(args).autoLoadModules().exec().exit();
     }
 }
--- springboot-rest/src/main/java/org/objectstyle/springboot/Main.java
+++ springboot-rest/src/main/java/org/objectstyle/springboot/Main.java
@@ -3,10 +3,20 @@ package org.objectstyle.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.lang.management.ManagementFactory;
+import java.time.LocalDateTime;
+
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
+
+        // measuring JVM start time
+        System.out.println(ManagementFactory.getRuntimeMXBean().getUptime());
+
+        // timestamping the app start point
+        System.out.println(LocalDateTime.now());
+
         SpringApplication.run(Main.class, args);
     }
 }
